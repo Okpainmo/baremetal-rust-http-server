af8c7210d8dc057cd5e39163e7abd4e0c26c2639
Okpainmo <okpainmoandrew@gmail.com>
2025-11-29 10:59:48 +0100

    init: cargo init...

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let x = 7;
+
+    println!("hello world the value of x is { }", x);
+
+    let mut y = 25;
+
+    println!("hello world the value of y is { }", y);
+
+    y = 12;
+
+    println!("the new value of y is { }", y);
+}
